96984413eebe0186dac010532051cf990753ff36
Kevin Kirn <kevin.kirn@primesystems.ch>
2017-01-16 18:54:39 +0100

    checked clean code ImageDataHolder

--- src/main/java/ch/fhnw/cuie/advancedimagepicker/ImageDataHolder.java
+++ src/main/java/ch/fhnw/cuie/advancedimagepicker/ImageDataHolder.java
@@ -3,22 +3,27 @@ package ch.fhnw.cuie.advancedimagepicker;
 import java.io.InputStream;
 
 /**
- * @author Hoang Tran
+ * This class is used to represent image data of a specific
+ * image received from a ImageService.
+ * 
+ * @author Hoang Tran <hoang.tran@students.fhnw.ch>
+ * @author Kevin Kirn <kevin.kirn@students.fhnw.ch>
  */
 public class ImageDataHolder {
-    private final InputStream thumbnailInputStream;
-    private final InputStream imageInputStream;
 
-    public ImageDataHolder(InputStream thumbnailInputStream, InputStream imageInputStream) {
-        this.thumbnailInputStream = thumbnailInputStream;
-        this.imageInputStream = imageInputStream;
-    }
+	private final InputStream thumbnailInputStream;
+	private final InputStream imageInputStream;
 
-    public InputStream getThumbnailInputStream() {
-        return thumbnailInputStream;
-    }
+	public ImageDataHolder(InputStream thumbnailInputStream, InputStream imageInputStream) {
+		this.thumbnailInputStream = thumbnailInputStream;
+		this.imageInputStream = imageInputStream;
+	}
 
-    public InputStream getImageInputStream() {
-        return imageInputStream;
-    }
+	public InputStream getThumbnailInputStream() {
+		return thumbnailInputStream;
+	}
+
+	public InputStream getImageInputStream() {
+		return imageInputStream;
+	}
 }
